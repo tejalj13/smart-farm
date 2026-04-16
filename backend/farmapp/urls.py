1af38e202e373f9f1f607f460d2c73f3640b2f5a
Tejal <joshitejal1303@gmail.com>
2026-04-16 18:45:09 +0100

    p5

--- backend/farmapp/urls.py
+++ backend/farmapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/sensor-data/', views.receive_data),
+    path('api/sensor-data/', views.get_sensor_data),
     path('', views.dashboard),
-    path('api/data/', views.get_sensor_data),
+    path('api/data/', views.receive_data),
 ]
